a5ed9f5f8184482b7b0770e25629581e5db83276
Friedrich von Never <friedrich@fornever.me>
2014-12-21 00:14:22 +0600

    Fix typos in comment.

--- Tracktor.Database/Connection.fs
+++ Tracktor.Database/Connection.fs
@@ -3,7 +3,7 @@
 open Npgsql
 
 // TODO: This is a temporary module and it should be here while NpgsqlConnection is not used anywhere else.
-// It should be referenced so,ewhere for Npgsql.dd to be copied to the output directory.
+// It should be referenced somewhere for the Npgsql.dll to be copied to the output directory.
 let dummy() = 
     let connection = new NpgsqlConnection("")
     ()
