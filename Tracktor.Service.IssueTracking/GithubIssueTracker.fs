50819ccc6033e085325896ab5114e2b170f6c566
Friedrich von Never <friedrich@fornever.me>
2015-01-02 21:44:39 +0600

    Add issue tracking project.

---
+++ Tracktor.Service.IssueTracking/GithubIssueTracker.fs
@@ -0,0 +1,8 @@
+﻿namespace Tracktor.Service.IssueTracking
+
+open Tracktor.Contracts
+
+type IIssueTracker =
+    abstract IssueActivated : IEvent<Issue>
+    abstract IssueDeactivated : IEvent<Issue>
+    abstract Start : unit -> unit
